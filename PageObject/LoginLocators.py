84c7363409408a9d7f83f4be1d311495b32412c6
SaurabhSuralikar <suralikarsaurabh@gmail.com>
2020-05-06 00:16:39 +0530

    Added POM Structure

---
+++ PageObject/LoginLocators.py
@@ -0,0 +1,5 @@
+# Page login Locators
+txt_LoginUserName="id:username"
+txt_LoginUserPwd="id:password"
+submit_btn_click ="xpath://button[@type='submit']"
+login_btn_click="xpath://a[text()='Login']"
